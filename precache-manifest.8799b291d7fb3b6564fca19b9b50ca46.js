5b6de18bcffd9bb2254505c7e2bad4b5445dca2c
PDogg66 <kunvtc6@gmail.com>
2020-05-05 22:31:58 +0700

    Updates

---
+++ precache-manifest.8799b291d7fb3b6564fca19b9b50ca46.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3674e08a29ce5764eba05dc34144c1e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1ecbb781fde0e50b27c",
+    "url": "/static/css/main.4993ed12.chunk.css"
+  },
+  {
+    "revision": "8e6265d6a343e0af77ab",
+    "url": "/static/js/2.e0024d75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e0024d75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ecbb781fde0e50b27c",
+    "url": "/static/js/main.8ffe2223.chunk.js"
+  },
+  {
+    "revision": "f5b6f99c50f30ee8a0df",
+    "url": "/static/js/runtime-main.c13a4c43.js"
+  },
+  {
+    "revision": "ecc9143b4b6c9ec07b2d96c86a523f6c",
+    "url": "/static/media/backgr1.ecc9143b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ca65f7458465d17783025d583666b646",
+    "url": "/static/media/ytbicon.ca65f745.png"
+  }
+]);
